3e7ca2da75151e3a822b4f288e9949688f9925c0
bartekkz <bartek.szmelczynski@gmail.com>
2019-07-01 15:45:32 +0200

    create about template

--- app/app.py
+++ app/app.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, render_template, url_for
+
+
 app = Flask("TwinsPage")
 
 @app.route('/')
 def hello_world():
   return "hello, world"
 
+
+
+@app.route('/Pito')
+def hello_pito():
+  return render_template('hello.html') 
 
 
 if __name__ == "__main__":
