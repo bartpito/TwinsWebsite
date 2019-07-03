a67d752c3374833c897a9695557017d91be39750
Piotrekszmel <piotr.szmelczynski@gmail.com>
2019-07-04 00:37:18 +0200

    workin on navigation bar

--- app/app.py
+++ app/app.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 
 from flask import Flask, render_template, url_for, redirect
+from flask_nav import Nav
+from flask_nav.elements import Navbar, Subgroup, View, Link, Text, Separator 
 
 app = Flask(__name__)
+nav = Nav(app)
 
+@nav.navigation("navbar")
+def create_navbar():
+  home_view = View("Home", 'index')
+  pito_view = View("About Pito", 'about', name="pito")
+  bart_view = View("About Bart", 'about', name="bart")
+  return Navbar("MySite", home_view, pito_view, bart_view)
 
 @app.route('/')
 def index():
