a13aba8d963e2373183622b8cb086ef07576208f
bartekkz <bartek.szmelczynski@gmail.com>
2019-07-11 10:40:49 +0200

    create basic skeleton of the "Projects" page

--- app/app.py
+++ app/app.py
@@ -2,36 +2,37 @@
 
 from flask import Flask, render_template, url_for, redirect
 from flask_nav import Nav
-from flask_nav.elements import Navbar, Subgroup, View, Link, Text, Separator 
+from flask_nav.elements import Navbar, Subgroup, View, Link, Text, Separator
 
 app = Flask(__name__)
 nav = Nav(app)
 
 
-<<<<<<< HEAD
-
-=======
->>>>>>> a76336b71bfd59cc986c83d1e2284cf5a4f97def
 @nav.navigation("navbar")
 def create_navbar():
-  home_view = View("Home", 'index')
-  pito_view = View("About Pito", 'about', name="pito")
-  bart_view = View("About Bart", 'about', name="bart")
-  return Navbar("MySite", home_view, pito_view, bart_view)
+    home_view = View("Home", 'index')
+    pito_view = View("About Pito", 'about', name="pito")
+    bart_view = View("About Bart", 'about', name="bart")
+    return Navbar("MySite", home_view, pito_view, bart_view)
 
 
 @app.route('/')
 def index():
-  return render_template('mainpage.html')
+    return render_template('mainpage.html')
+
 
 @app.route('/About/<name>')
 def about(name=None):
-  if name == 'pito':
-    return render_template('pito.html') 
-  elif name == 'bart':
-    return render_template('bart.html')
+    if name == 'pito':
+        return render_template('pito.html')
+    elif name == 'bart':
+        return render_template('bart.html')
 
+
+@app.route('/Projects')
+def projects():
+    return render_template('projects.html')
 
 
 if __name__ == "__main__":
-  app.run(debug=True, host='127.0.0.1')
+    app.run(debug=True, host='127.0.0.1')
