f391add2c19e907ab50ec30fb7541f263b11af8c
Piotrekszmel <piotr.szmelczynski@gmail.com>
2019-07-12 13:13:59 +0200

    working on adding Sentiment Analysis project

--- app/app.py
+++ app/app.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python3
 
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, request
+import pandas as pd
+import sys
+sys.path.append('projects/SentimentAnalysis/')
+from keras.models import load_model
+from keras.preprocessing.text import Tokenizer
+from cleaner import Cleaner
+from network import Network
+from newsparser import NewsParser
+import os
+import tensorflow as tf
 from flask_nav import Nav
 from flask_nav.elements import Navbar, Subgroup, View, Link, Text, Separator
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+tf.logging.set_verbosity(tf.logging.ERROR)
+
 
 app = Flask(__name__)
 nav = Nav(app)
@@ -38,6 +52,26 @@ def projects():
 def projectsPage():
     return render_template('projectsPage.html')
 
+@app.route('/Sentiment')
+def home():
+    return render_template('SentimentMainPage.html')
+
+
+@app.route('/predict', methods=['POST'])
+
+
+
+def predict():
+    network.connect_to_db('./data/news.db')
+    if request.method == 'POST':
+        topic = request.form['topic']
+        print(topic)
+        with graph.as_default():
+            prediction = network.predict_news(topic=topic, page_size=1, database=False)
+            print(network.parser.titles[0])
+            print(prediction)
+   
+    return render_template('result.html', prediction=prediction, text=network.parser.titles[0])
 
 if __name__ == "__main__":
     app.run(debug=True, host='127.0.0.1')
---
+++ app/projects/SentimentAnalysis/newsparser.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import requests
+import sqlite3
+from keras.models import load_model
+from cleaner import Cleaner
+from keras.preprocessing.text import Tokenizer
+
+
+class NewsParser:
+  def __init__(self):
+    pass
+
+
+  def get_data(self, topic, page_size, database=True):
+    self.titles = []
+    self.desc = []
+    self.content = []
+    url = f'https://newsapi.org/v2/everything?q={topic}' \
+                '&apiKey=c998e8aef8004548aed5c52d380e01c0' \
+                f'&pageSize={page_size}'
+    response = requests.get(url)
+    for page in range(page_size):
+      title = response.json()['articles'][page]['title']
+      desc = response.json()['articles'][page]['description']
+      content = response.json()['articles'][page]['content']
+
+      self.titles.append(title)
+      self.desc.append(desc)
+      self.content.append(content)
+  
+
+
+
+
+
+
+   
+
+    
+
