856f64e8c6ad6541f6fd358c3b122c54bf023171
Piotrekszmel <piotr.szmelczynski@gmail.com>
2019-07-15 16:57:50 +0200

    Compressing photo to make it load instantly

--- app/app.py
+++ app/app.py
@@ -1,42 +1,42 @@
 #!/usr/bin/env python3
-
-from flask import Flask, render_template, url_for, request
-import pandas as pd
 import sys
 sys.path.append('projects/SentimentAnalysis/')
-from keras.models import load_model
-from keras.preprocessing.text import Tokenizer
-from cleaner import Cleaner
-from network import Network
-from newsparser import NewsParser
 import os
+import pandas as pd
 import tensorflow as tf
-from flask_nav import Nav
+from newsparser import NewsParser
+from network import Network
+from cleaner import Cleaner
+from keras.preprocessing.text import Tokenizer
+from keras.models import load_model
 from flask_nav.elements import Navbar, Subgroup, View, Link, Text, Separator
+from flask_nav import Nav
+from flask import Flask, render_template, url_for, request
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 tf.logging.set_verbosity(tf.logging.ERROR)
 
 
 app = Flask(__name__)
 nav = Nav(app)
 
+
 maxlen = 100
 max_words = 20000
 emb_size = 50
 
-network = Network(max_words, maxlen, emb_size, None, False, 'data/my_model_3.h5')
+network = Network(
+    max_words,
+    maxlen,
+    emb_size,
+    None,
+    False,
+    'data/my_model_3.h5')
 
 global graph
 graph = tf.get_default_graph()
-
-@nav.navigation("navbar")
-def create_navbar():
-    home_view = View("Home", 'index')
-    pito_view = View("About Pito", 'about', name="pito")
-    bart_view = View("About Bart", 'about', name="bart")
-    project_view = View("Projects", )
-    return Navbar("MySite", home_view, pito_view, bart_view)
 
 
 @app.route('/')
@@ -56,26 +56,29 @@ def about(name=None):
 def projectsPage():
     return render_template('projectsPage.html')
 
+
 @app.route('/Sentiment')
 def sentiment():
     return render_template('SentimentMainPage.html')
 
 
 @app.route('/Predict', methods=['POST'])
-
-
-
 def predict():
     network.connect_to_db('./data/news.db')
     if request.method == 'POST':
         topic = request.form['topic']
         print(topic)
         with graph.as_default():
-            prediction = network.predict_news(topic=topic, page_size=1, database=False)
+            prediction = network.predict_news(
+                topic=topic, page_size=1, database=False)
             print(network.parser.titles[0])
             print(prediction)
-   
-    return render_template('SentimentResults.html', prediction=prediction, text=network.parser.titles[0])
+
+    return render_template(
+        'SentimentResults.html',
+        prediction=prediction,
+        text=network.parser.titles[0])
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
