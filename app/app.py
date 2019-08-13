e54647f8ac3ddba547b9b0ea8ec9750c1ff1058c
bartekkz <bartek.szmelczynski@gmail.com>
2019-08-13 19:41:44 +0200

    clean up folders

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import sys
 sys.path.append('projects/SentimentAnalysis/')
+sys.path.append('projects/chatbot/')
 import os
 import pandas as pd
 import tensorflow as tf
@@ -14,6 +15,8 @@ from flask_nav import Nav
 from flask import Flask, render_template, url_for, request
 from flask_socketio import SocketIO, send
 from flask import request
+import warnings
+warnings.filterwarnings('ignore')
 
 
 
@@ -101,7 +104,16 @@ def learningPage():
 
 @app.route('/chatbot')
 def chatbot():
+    #os.system('make action')
     return render_template('chatbot.html')
+
+@app.route('/fullMode')
+def fullMode():
+    return render_template('fullMode.html')
+
+@app.route('/winMode')
+def winMode():
+    return render_template('winMode.html')
 
 @socketio.on('message')
 def handleMessage(msg):
@@ -109,6 +121,6 @@ def handleMessage(msg):
     send(msg, broadcast=True)
 
 if __name__ == "__main__":
-    socketio.run(app, host='0.0.0.0', debug=True, port=5002)
+    socketio.run(app, host='0.0.0.0', debug=True, port=5003)
     #app.run(debug=True, host='localhost', port=5002)
 
