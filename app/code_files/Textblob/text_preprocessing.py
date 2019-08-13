9415ab76c0bad4d5cebd40f0ae0e0ef1fbc40cdc
Piotrekszmel <piotr.szmelczynski@gmail.com>
2019-08-13 16:56:57 +0200

    added POS code for textblob

--- app/code_files/Textblob/text_preprocessing.py
+++ app/code_files/Textblob/text_preprocessing.py
@@ -1,7 +1,13 @@
 from textblob import TextBlob
 
-blob = TextBlob("Internet is a great place to learn data science. It helps community through blogs, hackathons, discussions, etc .")
+blob = TextBlob("Internet is a great place to learn data science. It helps community through blogs, hackathons, discussions, etc.")
+
+#Tokenization
 sent = blob.sentences
 words = blob.words
-print(sent)
-print("\n\n", words)
+
+#Part-of-speech tagging
+pos = blob.tags
+print(pos)
+
+
